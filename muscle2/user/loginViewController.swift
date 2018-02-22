892ba8a9def5b7a3074a59c7a4b64ceab57d6060
sasakazu <dropout.college2014@gmail.com>
2018-02-22 19:08:56 +0900

    email pass outlet action done....

---
+++ muscle2/user/loginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  loginViewController.swift
+//  muscle2
+//
+//  Created by 笹倉 一也 on 2018/02/22.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+        
+    }
+    
+
+    
+    
+    
+}
---
+++ muscle2/user/signupViewController.swift
@@ -0,0 +1,36 @@
+//
+//  signupViewController.swift
+//  muscle2
+//
+//  Created by 笹倉 一也 on 2018/02/22.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class signupViewController: UIViewController {
+
+
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passConfirmationTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signupTapped(_ sender: Any) {
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+    
+
+
+}
