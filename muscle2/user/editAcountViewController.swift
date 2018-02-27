2c61f30240c9d3477198b73dc20056b25464bcbc
sasakazu <dropout.college2014@gmail.com>
2018-02-27 19:13:40 +0900

    edit account file setting done....

---
+++ muscle2/user/editAcountViewController.swift
@@ -0,0 +1,41 @@
+//
+//  editAcountViewController.swift
+//  muscle2
+//
+//  Created by 笹倉 一也 on 2018/02/27.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class editAcountViewController: UIViewController {
+
+    
+    @IBOutlet weak var userNameEditTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func acountEditImage(_ sender: Any) {
+    
+    
+    }
+    
+    
+    @IBAction func acountButtonTapped(_ sender: Any) {
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
