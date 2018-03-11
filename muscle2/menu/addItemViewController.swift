f6168a475bb43efff30257f997003f9bbc0a9d39
sasakazu <sasakura.company@gmail.com>
2018-03-11 19:59:45 +0900

    add item view done....

---
+++ muscle2/menu/addItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  addItemViewController.swift
+//  muscle2
+//
+//  Created by 笹倉 一也 on 2018/03/11.
+//  Copyright © 2018年 笹倉 一也. All rights reserved.
+//
+
+import UIKit
+
+class addItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
